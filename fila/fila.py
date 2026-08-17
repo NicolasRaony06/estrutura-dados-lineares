6ac931b7bd08382bb2fc867880201aabfe2e1f67
Nicolas Raony <nicolas.raony06@gmail.com>
2026-08-17 17:33:01 -0300

    Add: estrutura de dado fila

--- fila/fila.py
+++ fila/fila.py
@@ -1,17 +1,66 @@
 class Item:
-    def __init__(self, valor, prev):
+    def __init__(self, valor, next = None):
         self.valor = valor
-        self.prev = prev
+        self._next = next
 
     def __str__(self) -> str:
         return f"{self.valor}"
 
 class Fila:
     def __init__(self):
-        self.__primeiro = None
+        self.__inicio = None
+        self.__fim = None
+
+    def inicio(self):
+        return self.__inicio
+
+    def fim(self):
+        return self.__fim
 
     def inserir(self, valor):
-        pass
+        item = Item(valor)
+        if not self.__inicio:
+            self.__inicio = item
+            return
+
+        if not self.__fim:
+            self.__inicio._next = item
+            self.__fim = item
+            return
+
+        self.__fim._next = item
+        self.__fim = item
 
     def excluir(self):
-        pass
+        if self.__inicio:
+            if self.__inicio == self.__fim:
+                self.__fim = None
+            self.__inicio = self.__inicio._next
+
+
+if __name__ == "__main__":
+    fila = Fila()
+
+    fila.inserir(3)
+    print(fila.inicio())
+    print(fila.fim(), "\n")
+
+    fila.inserir(4)
+    print(fila.inicio())
+    print(fila.fim(), "\n")
+
+    fila.inserir(5)
+    print(fila.inicio())
+    print(fila.fim(), "\n")
+
+    fila.excluir()
+    print(fila.inicio())
+    print(fila.fim(), "\n")
+
+    fila.excluir()
+    print(fila.inicio())
+    print(fila.fim(), "\n")
+    
+    fila.excluir()
+    print(fila.inicio())
+    print(fila.fim(), "\n")
