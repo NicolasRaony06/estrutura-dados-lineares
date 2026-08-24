103849b01dd8417c001d1a2f50684710e9bd9be7
NicolasRaony06 <nicolas.raony06@gmail.com>
2026-08-24 14:18:35 -0300

    Add: metodos de inserção ordenada e listar nós

--- polinomio/estruturas/lista_encadeada.py
+++ polinomio/estruturas/lista_encadeada.py
@@ -1,5 +1,46 @@
 class No:
-    pass
+    def __init__(self, coeficiente:float, grau:int, next = None):
+        self.coeficiente = coeficiente
+        self.grau = grau
+        self.next = next
+
+    def __str__(self):
+        return f"{self.coeficiente},{self.grau}"
 
 class Lista:
-    pass
+    def __init__(self):
+        self.header = None
+
+    def inserir(self, coeficiente:float, grau:int):
+        novo_no = No(coeficiente, grau)
+        
+        if not self.header:
+            self.header = novo_no
+            return
+
+        no_atual = self.header
+        while no_atual:
+            if no_atual.grau >= novo_no.grau:
+                if not no_atual.next:
+                    no_atual.next = novo_no
+                    return 
+                
+                if no_atual.next.grau <= novo_no.grau:
+                    novo_no.next = no_atual.next
+                    no_atual.next = novo_no
+                    return
+            else:
+                novo_no.next = no_atual
+                self.header = novo_no
+                return
+             
+            no_atual = no_atual.next
+
+    def mostrarAll(self):
+        nos = []
+        no = self.header
+        while no:
+            nos.append(no)
+            no = no.next
+        return nos
+
