3aa8fb952898a9df7c9ed2694802a91af2f7bcc8
Nicolas Raony <nicolas.raony06@gmail.com>
2026-08-27 14:17:11 -0300

    Add: metodos grau|tamanho e avaliacao

--- polinomio/polinomio.py
+++ polinomio/polinomio.py
@@ -10,8 +10,23 @@ class Polinomio:
     def excluirTermo(self, coeficiente:float, grau:int):
         self.termos.excluir(coeficiente, grau)
 
+    def grau(self):
+        return self.termos.header.grau
+
+    def tamanho(self):
+        return self.termos.tamanho()
+
+    def avaliacao(self, valor_de_x:float|int):
+        '''Recebe um valor para a variável X e retorna o resultado do polinômio.'''
+        resultado = 0
+        termo = self.termos.header
+        while termo:
+            resultado += termo.coeficiente * (valor_de_x ** termo.grau)
+            termo = termo.next
+        return resultado
+
     def __str__(self):
-        #polinomio = ""
+        polinomio = ""
         termos = self.termos.mostrarAll()
         for termo in termos:
             if termo.coeficiente > 0:
