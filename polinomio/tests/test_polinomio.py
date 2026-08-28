826c9718e4099fd91b6178e657f418415edc2390
Nicolas Raony <nicolas.raony06@gmail.com>
2026-08-28 17:09:56 -0300

    Add: teste de simplificação

--- polinomio/tests/test_polinomio.py
+++ polinomio/tests/test_polinomio.py
@@ -18,8 +18,21 @@ print(polinomio.tamanho())
 
 #teste atribuição de valor X
 print(polinomio.definirX(2))
+print()
 
-#teste simplificação
+#teste simplificação para coeficiente 0
 polinomio.inserirTermo(0, 6)
+print(polinomio, '\n')
 polinomio.simplificar()
 print(polinomio)
+print()
+
+#teste para termos de mesmo grau
+polinomio.inserirTermo(2, 5)
+polinomio.inserirTermo(6, 1)
+polinomio.inserirTermo(-1, 5)
+polinomio.inserirTermo(3.5, 1)
+print(polinomio, '\n')
+polinomio.simplificar()
+print(polinomio)
+
