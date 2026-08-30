0b0095ecf5ab4e4f829463f99ceb862506c0552c
NicolasRaony06 <nicolas.raony06@gmail.com>
2026-08-30 15:04:24 -0300

    Update: função ler arquivo caminho default

--- exercicio-polinomio/main.py
+++ exercicio-polinomio/main.py
@@ -1,9 +1,14 @@
 from polinomio import Polinomio
 
-def lerArquivo(arquivo:str = "entrada.txt"):
+def lerArquivo(caminho_arquivo:str = None):
+    '''Procura por padrão um arquivo .txt em /data, caso não seja especificado um caminho. Retorna um dicionário contendo as instruções e os polinomios.'''
+    if not caminho_arquivo:
+        from glob import glob
+        caminho_arquivo = (glob("data/*.txt"))[0]
+
     instrucoes = {}
     comandos = ('g', 't', '+', '-', '*', 'a', 'p')
-    with open(rf"data/{arquivo}") as arquivo:
+    with open(caminho_arquivo) as arquivo:
         instrucao_prev = ''
         for index, line in enumerate(arquivo):
             if line.strip().lower() in comandos:
@@ -14,4 +19,6 @@ def lerArquivo(arquivo:str = "entrada.txt"):
                 instrucao_prev = f"instrucao{index}"
                 continue
             instrucoes[instrucao_prev]['polinomios'].append(line.rstrip('\n'))
+    return instrucoes
 
+lerArquivo()
