732a93e5b66d441d76b1632d0a147a34017ce37e
Nicolas Raony <nicolas.raony06@gmail.com>
2026-09-16 16:21:00 -0300

    Add: metodo writeBin

--- exercicio-compactador-DNAstring/compactador.py
+++ exercicio-compactador-DNAstring/compactador.py
@@ -13,9 +13,11 @@ class Compactador:
         self.__bytes = None
 
     def readBin(self):
+        print(self.file)
         if self.file.suffix == '.bin':
             with open(self.file, 'rb') as file:
                 self.__bytes = file.read()
+            return self.__bytes
         else: raise FileNotFoundError("File extension must be '.bin'.")
 
     def readTxt(self):
@@ -23,7 +25,13 @@ class Compactador:
             with open(self.file) as file:
                 self.__string = file.read().strip()
             self.__string = self.__string.replace('\n', '').replace(' ', '')
+            return self.__string
         else: raise FileNotFoundError("File extension must be '.txt'.")
+
+    def writeBin(self):
+        file = self.file.with_name(self.file.stem + '_compressed.bin')
+        with open(file, 'wb') as file:
+            file.write(self.__bytes)
 
     def __createHeader(self, genes_counter: int):
         return genes_counter.to_bytes(4, 'big')
@@ -51,7 +59,9 @@ class Compactador:
             binary_values.append(binary_value)
         
         self.__bytes = self.__createHeader(genes_counter) + bytes(binary_values)
-        return self.__bytes
+        self.writeBin()
+        return True
         
+    
 
     
--- exercicio-compactador-DNAstring/main.py
+++ exercicio-compactador-DNAstring/main.py
@@ -1,13 +1,6 @@
 from compactador import Compactador
 
 comp = Compactador('arquivos/example2.txt')
-
-byte = comp.compress()
-
-print(list(byte))
-
-for i in byte:
-    print(format(i, '08b'))
-
+comp.compress()
 
 
